5d365e8bed48c22648a129be3331cbc1518783bb
Yazdun <yazduntube@gmail.com>
2022-03-27 17:04:31 +0430

    forms : added create form validators

--- src/utils/forms.js
+++ src/utils/forms.js
@@ -25,3 +25,101 @@ export const PasswordInput = {
     },
   },
 }
+
+export const TitleInput = {
+  name: 'title',
+  label: 'title',
+  type: 'text',
+  id: 'title',
+  placeholder: 'e.g. title',
+  validation: {
+    required: {
+      value: true,
+      message: 'title is required !',
+    },
+    minLength: {
+      value: 2,
+      message: 'min length is 3 characters',
+    },
+    maxLength: {
+      value: 50,
+      message: 'max length is 50 characters',
+    },
+  },
+}
+
+export const descriptionInput = {
+  name: 'description',
+  label: 'description',
+  type: 'text',
+  id: 'description',
+  placeholder: 'e.g. description',
+  validation: {
+    required: {
+      value: true,
+      message: 'description is required !',
+    },
+    minLength: {
+      value: 2,
+      message: 'min length is 2 characters',
+    },
+  },
+}
+
+export const ContentInput = {
+  name: 'content',
+  label: 'content',
+  type: 'text',
+  id: 'content',
+  placeholder: 'e.g. content',
+  validation: {
+    required: {
+      value: true,
+      message: 'content is required !',
+    },
+    minLength: {
+      value: 10,
+      message: 'min length is 10 characters',
+    },
+  },
+}
+
+export const KeywordsInput = {
+  name: 'keywords',
+  label: 'keywords',
+  type: 'text',
+  id: 'keywords',
+  placeholder: 'e.g. keywords',
+}
+
+export const ImageInput = {
+  name: 'image',
+  label: 'image',
+  type: 'text',
+  id: 'image',
+  placeholder: 'https://image.com',
+  validation: {
+    pattern: {
+      value:
+        /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/,
+      message: 'not a valid url',
+    },
+  },
+}
+
+export const UrlInput = header => {
+  return {
+    name: header,
+    label: header,
+    type: 'text',
+    id: header,
+    placeholder: 'https://example.com',
+    validation: {
+      pattern: {
+        value:
+          /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/,
+        message: 'not a valid url',
+      },
+    },
+  }
+}
