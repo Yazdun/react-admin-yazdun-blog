f012efefa0240660f604d3fcc784dec4c169394a
Yazdun <yazduntube@gmail.com>
2022-03-31 22:08:25 +0430

    markdown : made progress

--- src/components/markdown/index.jsx
+++ src/components/markdown/index.jsx
@@ -4,6 +4,9 @@ import { dracula } from 'react-syntax-highlighter/dist/esm/styles/prism'
 import s from './styles.module.scss'
 
 export const Markdown = ({ markdown }) => {
+  // api doesnt return < > so I had to use regex to replace it in the markdown
+  const content = markdown?.replace(/&lt;/g, '<').replace(/&gt;/g, '>')
+
   if (!markdown) {
     return (
       <div className={s.container}>
@@ -15,7 +18,7 @@ export const Markdown = ({ markdown }) => {
   return (
     <div className={s.container}>
       <ReactMarkdown
-        children={markdown}
+        children={content}
         components={{
           code({ node, inline, className, children, ...props }) {
             const match = /language-(\w+)/.exec(className || '')
